4825b8831c706ba88fb72a5492dcc545a3535643
ipritamdatta <pritamewu@gmail.com>
2020-05-24 23:51:44 +0600

    Updates

---
+++ precache-manifest.b68d183bb33c4fc8daf7e64c663517cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e75b21f5a0199afa90a021f54cc647",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e89a9767be0fbf675bb",
+    "url": "/monsters-rolodex/static/css/main.0203ffcc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e89a9767be0fbf675bb",
+    "url": "/monsters-rolodex/static/js/main.1c045c86.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
